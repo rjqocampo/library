533974b70dae725920a8f7dcb50baa1903bfd632
RJ Ocampo <rjqocampo@gmail.com>
2023-08-19 14:39:32 +0800

    Add remove and toggleHasRead features

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-const buttonAddBook = document.querySelector('.button-add-book');
-const buttonCloseDialog = document.querySelector('.button-close-dialog');
-const buttonSubmit = document.querySelector('.button-submit');
-const dialog = document.querySelector('dialog')
-const containerBooks = document.querySelector('.container-books');
-const inputTitle = document.getElementById('form-title');
-const inputAuthor = document.getElementById('form-author');
-const inputPages = document.getElementById('form-pages');
-const inputHasRead = document.getElementById('form-has-read');
-
-buttonAddBook.addEventListener('click', () => {
-  dialog.showModal();
-})
-buttonCloseDialog.addEventListener('click', () => {
-  dialog.close();
-})
-
-buttonSubmit.addEventListener('click', (event) => {
-  event.preventDefault();
-  addBookToLibrary();
-  clearDisplayBooks();
-  dialog.close();
-  displayBooks();
-})
-
-
 const myLibrary = [
   {
-      title: 'A Song of Ice: Adventures of Hodor Part III',
+      title: 'A Song of Ice and Fire: Adventures of Hodor and Gandalf Part III Electric Boogaloo',
       author: 'George R. R. Martin',
       pages: 300,
       "has read": false,
@@ -50,6 +24,33 @@ const myLibrary = [
     "has read": true,
   },
 ];
+
+const buttonAddBook = document.querySelector('.button-add-book');
+const buttonCloseDialog = document.querySelector('.button-close-dialog');
+const buttonSubmit = document.querySelector('.button-submit');
+const dialog = document.querySelector('dialog')
+const containerBooks = document.querySelector('.container-books');
+const inputTitle = document.getElementById('form-title');
+const inputAuthor = document.getElementById('form-author');
+const inputPages = document.getElementById('form-pages');
+const inputHasRead = document.getElementById('form-has-read');
+let buttonRemove;
+
+buttonAddBook.addEventListener('click', () => {
+  dialog.showModal();
+})
+
+buttonCloseDialog.addEventListener('click', () => {
+  dialog.close();
+})
+
+buttonSubmit.addEventListener('click', (event) => {
+  addBookToLibrary();
+  clearDisplayBooks();
+  displayBooks();
+  dialog.close();
+  event.preventDefault();
+})
 
 function Book(title, author, pages, hasRead) {
   this.title = title,
@@ -77,10 +78,29 @@ function clearDisplayBooks() {
   while (containerBooks.firstChild) {
     containerBooks.removeChild(containerBooks.firstChild);
   }
+  buttonRemove = null;
+}
+
+function deleteBook(event) {
+  myLibrary.splice((event.target.parentNode.getAttribute('data-index')), 1);
+  clearDisplayBooks();
+  displayBooks();
+}
+
+function toggleHasRead(event) {
+  let index = event.target.parentNode.getAttribute('data-index');
+
+  if (myLibrary[index]['has read']) {
+    myLibrary[index]['has read'] = false;
+  } else {
+    (myLibrary[index]['has read']) = true;
+  }
+  clearDisplayBooks();
+  displayBooks();
 }
 
 function displayBooks() {
-  myLibrary.forEach((book) => {
+  myLibrary.forEach((book, index) => {
     let cardBook = document.createElement('div');
     let cardTitle = document.createElement('p');
     let cardAuthor = document.createElement('p');
@@ -89,21 +109,31 @@ function displayBooks() {
     let cardRemove = document.createElement('button');
 
     cardBook.setAttribute('class', 'book');
+    cardBook.setAttribute('data-index', `${index}`);
     cardTitle.setAttribute('class', 'book__title');
     cardAuthor.setAttribute('class', 'book__author');
     cardPages.setAttribute('class', 'book__pages');
-    cardHasRead.setAttribute('class', 'book__has-read') ;
     cardRemove.setAttribute('class', 'book__remove');
 
     cardTitle.textContent = `"${book.title}"`;
-    cardAuthor.textContent = book.author;
+    cardAuthor.textContent = `By ${book.author}`;
     cardPages.textContent = `${book.pages} pages`;
-    cardHasRead.textContent = book['has read'] ? 'Read' : "Not read";
     cardRemove.textContent = 'Remove';
+
+    if (book['has read']) {
+      cardHasRead.textContent = 'Read';
+      cardHasRead.setAttribute('class', 'book__has-read');
+    } else {
+      cardHasRead.textContent = "Not read";
+      cardHasRead.setAttribute('class', 'book__has-read--false')
+    }
 
     cardBook.append(cardTitle, cardAuthor, cardPages, cardHasRead, cardRemove);
 
     containerBooks.appendChild(cardBook);
+
+    cardHasRead.addEventListener('click', toggleHasRead);
+    cardRemove.addEventListener('click', deleteBook);
   })
 }
 
